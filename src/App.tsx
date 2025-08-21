c4bc5a848c960b27ffa86446ec965b5a8715190c
newdana01 <newdana01@gmail.com>
2025-08-21 17:36:23 +0900

    feat: App.tsx에 Layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [darkMode, setDarkMode] = useState(true);
+
+  // 다크 모드 토글
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout>
+      <h1 className="text-2xl font-bold">테스트</h1>
+      <p className="mt-4">테스트</p>
+    </Layout>
+    // <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-primary-dark">
+    //   <button
+    //     onClick={toggleDarkMode}
+    //     className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800"
+    //   >
+    //     {darkMode ? "라이트 모드로 전환" : "다크 모드로 전환"}
+    //   </button>
+    //   <h1 className="text-3xl font-bold text-black dark:text-white">
+    //     다크 모드 테스트
+    //   </h1>
+    // </div>
   );
 }
 
