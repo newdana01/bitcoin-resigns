2587682f6744bd4414fe720f2a06df53b2bab1a0
newdana01 <newdana01@gmail.com>
2025-08-21 17:32:21 +0900

    feat: Navbar 컴포넌트 추가

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,50 @@
+import { Menu } from "lucide-react";
+
+type NavbarProps = {
+  onMenuClick: () => void;
+};
+
+export default function Navbar({ onMenuClick }: NavbarProps) {
+  return (
+    <header className="w-full text-white shadow">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+        {/* 로고 */}
+        <div className="text-xl font-bold mr-10">
+          <img src="/images/logo.png" alt="logo" className="w-10 h-auto" />
+        </div>
+
+        {/* 데스크탑 네비게이션 */}
+        <div className="hidden lg:flex flex-1 justify-start">
+          <nav className="hidden lg:flex space-x-6">
+            <a href="#" className="hover:text-primary-yellow font-semibold">
+              Bitcoin Price
+            </a>
+            <a href="#" className="hover:text-primary-yellow font-semibold">
+              How Bubbly?
+            </a>
+            <a href="#" className="hover:text-primary-yellow font-semibold">
+              What If?
+            </a>
+            <a href="#" className="hover:text-primary-yellow font-semibold">
+              Satoshi Scope
+            </a>
+            <a href="#" className="hover:text-primary-yellow font-semibold">
+              Get Your Moves
+            </a>
+            <a href="#" className="hover:text-primary-yellow font-semibold">
+              Coin Voices
+            </a>
+          </nav>
+        </div>
+
+        {/* 모바일/패드 햄버거 버튼 */}
+        <button
+          className="lg:hidden p-2 rounded hover:text-primary-yellow background-transparent"
+          onClick={onMenuClick}
+        >
+          <Menu size={24} />
+        </button>
+      </div>
+    </header>
+  );
+}
