ecb27323ab9f56ae61b50a3c3ba747120e8a3c75
newdana01 <newdana01@gmail.com>
2025-08-26 16:55:51 +0900

    feat: InfoTooltip 컴포넌트 추가.\n props: content. 툴팁에 표시할 컨텐츠 내용

---
+++ src/common/components/InfoTooltip.tsx
@@ -0,0 +1,28 @@
+import { Info } from "lucide-react";
+import { Tooltip } from "react-tooltip";
+
+interface InfoTooltipProps {
+  content: string;
+}
+
+export default function InfoTooltip({ content }: InfoTooltipProps) {
+  return (
+    <div className="inline-block relative h-4">
+      <Info
+        size={12}
+        data-tooltip-id="info-tooltip"
+        data-tooltip-content={content}
+        className="hover:text-primary-orange text-slate-400"
+      />
+      <Tooltip
+        id="info-tooltip"
+        className="whitespace-pre-line max-w-xs !opacity-100 z-50"
+        style={{
+          backgroundColor: "rgb(31, 41, 55)",
+          color: "#fff",
+          opacity: 1,
+        }}
+      ></Tooltip>
+    </div>
+  );
+}
