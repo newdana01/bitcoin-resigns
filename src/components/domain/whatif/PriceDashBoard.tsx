694be73276f022ac7c001f8912c156c969c4bcfa
newdana01 <newdana01@gmail.com>
2025-08-26 17:46:11 +0900

    feat: hidden 속성 추가. peer radio 버튼 선택시 block

--- src/components/domain/whatif/PriceDashBoard.tsx
+++ src/components/domain/whatif/PriceDashBoard.tsx
@@ -2,7 +2,7 @@ import PriceInput from "./PriceInput";
 
 export default function PriceDashBoard() {
   return (
-    <div className="peer-checked/calculatePrice">
+    <div className="hidden peer-checked/calculatePrice:block my-2">
       <fieldset className="group">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-3">
           <PriceInput name="buyPrice" label="Buy Price" />
