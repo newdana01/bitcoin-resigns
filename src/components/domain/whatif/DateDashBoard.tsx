88e6b1d64b14d2d6c4834840dc30c76d9c0aceba
newdana01 <newdana01@gmail.com>
2025-08-26 19:11:36 +0900

    feat: 날짜 변동 시 getPrice로 해당 날짜의 시세 가져오는 로직 추가

--- src/components/domain/whatif/DateDashBoard.tsx
+++ src/components/domain/whatif/DateDashBoard.tsx
@@ -1,27 +1,53 @@
-import { ArrowRight } from "lucide-react";
 import CalendarPicker from "./CalendarPicker";
 import PriceInput from "./PriceInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Label from "../../../common/components/Label";
+import getPrice from "../../../common/functions/getPrice";
 
 export default function DateDashBoard() {
-  const [date, setDate] = useState<Date>();
+  const [buyDate, setBuyDate] = useState<Date | null>(null);
+  const [sellDate, setSellDate] = useState<Date | null>(null);
+  const [buyPrice, setBuyPrice] = useState<number | null>(null);
+  const [sellPrice, setSellPrice] = useState<number | null>(null);
+
+  const fetchData = async (
+    date: Date | null,
+    setPrice: (price: number) => void,
+  ) => {
+    try {
+      const { closes } = await getPrice("BTCUSDT", "1d", 1, date?.getTime());
+      if (closes.length > 0) {
+        setPrice(Number(closes[0].toFixed(2)));
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(buyDate, setBuyPrice);
+  }, [buyDate]);
+
+  useEffect(() => {
+    fetchData(sellDate, setSellPrice);
+  }, [sellDate]);
+
   return (
     <div className="hidden peer-checked/calculateDate:block">
       <div className="my-2">
         <Label label="Buy Date" />
         <div className="flex">
-          <CalendarPicker />
+          <CalendarPicker selectedDate={buyDate} onSelectDate={setBuyDate} />
           <span className="mx-2 leading-[2.5rem]">→</span>
-          <PriceInput name="buyDate" disabled={true} />
+          <PriceInput name="buyDate" disabled={true} propValue={buyPrice} />
         </div>
       </div>
       <div className="my-2">
         <Label label="Sell Date" />
         <div className="flex">
-          <CalendarPicker />
+          <CalendarPicker selectedDate={sellDate} onSelectDate={setSellDate} />
           <span className="mx-2 leading-[2.5rem]">→</span>
-          <PriceInput name="sellDate" disabled={true} />
+          <PriceInput name="sellDate" disabled={true} propValue={sellPrice} />
         </div>
       </div>
       <div>
