e4ee5cdf379cdddaec563d4674fc4b764913be73
newdana01 <newdana01@gmail.com>
2025-08-27 01:47:06 +0900

    feat: 스타일 변경 - 최소 높이 설정, 수익 배경색 변경

--- src/components/domain/whatif/Result.tsx
+++ src/components/domain/whatif/Result.tsx
@@ -5,7 +5,7 @@ interface ResultProps {
 
 export default function Result({ profitLoss, totalInvestment }: ResultProps) {
   return (
-    <div className="border border-slate-700 rounded-md p-4 h-full flex flex-col justify-between">
+    <div className="border border-slate-700 rounded-md p-4 max-h-[17rem] h-full flex flex-col justify-between">
       <h2 className="text-lg font-bold mb-2 md:text-2xl">
         If you bought at this price, now...
       </h2>
@@ -13,7 +13,7 @@ export default function Result({ profitLoss, totalInvestment }: ResultProps) {
         <div className="my-2">
           <span className="text-sm text-slate-700">Profit/Loss </span>
           <span
-            className={`block w-fit text-lg md:text-2xl rounded-md px-1 ${profitLoss && profitLoss < 0 ? "bg-red-950" : "bg-green-500"} ${profitLoss && profitLoss < 0 ? "text-red-500" : "text-green-500"}`}
+            className={`block w-fit text-lg md:text-2xl rounded-md px-1 ${profitLoss && profitLoss < 0 ? "bg-red-950" : "bg-green-950"} ${profitLoss && profitLoss < 0 ? "text-red-500" : "text-green-500"}`}
           >
             ${profitLoss ? profitLoss.toLocaleString("en-US") : 0}
           </span>
