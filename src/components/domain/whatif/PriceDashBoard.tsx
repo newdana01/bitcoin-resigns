d531538a4c6c994d8f79bada3fe790e997102254
newdana01 <newdana01@gmail.com>
2025-08-27 01:47:45 +0900

    feat: 스타일 변경 - md 이상 화면에서 최대 너비 지정, label 변경

--- src/components/domain/whatif/PriceDashBoard.tsx
+++ src/components/domain/whatif/PriceDashBoard.tsx
@@ -2,14 +2,14 @@ import PriceInput from "./PriceInput";
 
 export default function PriceDashBoard() {
   return (
-    <div className="hidden peer-checked/calculatePrice:block my-2">
+    <div className="hidden peer-checked/calculatePrice:block my-2 md:max-w-[31rem]">
       <fieldset className="group">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-2">
           <PriceInput name="buyPrice" label="Buy Price" />
           <PriceInput name="sellPrice" label="Sell Price" />
         </div>
-        <div className="mb-3">
-          <PriceInput name="investment" label="Investment" />
+        <div className="mb-2">
+          <PriceInput name="investment" label="Investment Amount" />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <PriceInput name="investmentFee" label="Invetstment Fee" />
