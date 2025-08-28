3fb56361571dd59770e7bbe8318cc2108ae4b97b
newdana01 <newdana01@gmail.com>
2025-08-28 17:02:48 +0900

    feat: NewsList 컴포넌트 추가

---
+++ src/components/domain/news/NewsList.tsx
@@ -0,0 +1,9 @@
+import SectionName from "../../../common/components/SectionName";
+
+export default function NewsList() {
+  return (
+    <div>
+      <SectionName name="Satosi Scope"></SectionName>
+    </div>
+  );
+}
