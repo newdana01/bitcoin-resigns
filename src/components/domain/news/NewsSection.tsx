e1d5bb9495aaec703be441b15916631b24f5ee78
newdana01 <newdana01@gmail.com>
2025-08-28 17:03:38 +0900

    feat: 하위 섹션 컴포넌트 추가

--- src/components/domain/news/NewsSection.tsx
+++ src/components/domain/news/NewsSection.tsx
@@ -1,3 +1,11 @@
+import GetYourMoves from "./GetYourMoves";
+import NewsList from "./NewsList";
+
 export default function NewsSection() {
-  return <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <NewsList />
+      <GetYourMoves />
+    </div>
+  );
 }
