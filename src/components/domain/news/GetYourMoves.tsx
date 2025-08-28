f7d74d3390a90f0fa98458332e299926f7e156ff
newdana01 <newdana01@gmail.com>
2025-08-28 17:02:23 +0900

    feat: GetYourMoves 컴포넌트 추가

---
+++ src/components/domain/news/GetYourMoves.tsx
@@ -0,0 +1,9 @@
+import SectionName from "../../../common/components/SectionName";
+
+export default function GetYourMoves() {
+  return (
+    <div>
+      <SectionName name="Get Your Moves!" />
+    </div>
+  );
+}
