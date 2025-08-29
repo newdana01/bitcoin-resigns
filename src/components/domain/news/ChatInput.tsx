65a8e9229a61503a06dcc36ca6f3f7266cb8663d
newdana01 <newdana01@gmail.com>
2025-08-29 17:19:40 +0900

    feat: 채팅 입력란 생성

---
+++ src/components/domain/news/ChatInput.tsx
@@ -0,0 +1,47 @@
+import { useCallback, useState } from "react";
+
+interface ChatInputProps {
+  onSendMessage: (message: string) => void;
+}
+
+export default function ChatInput({ onSendMessage }: ChatInputProps) {
+  const [message, setMesssage] = useState("");
+  const handleSubmit = useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+      if (message.trim()) {
+        onSendMessage(message);
+        setMesssage("");
+      }
+    },
+    [message, onSendMessage],
+  );
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <div className="flex">
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => {
+                setMesssage(e.target.value);
+              }}
+              placeholder="Get tips from AI crypto experts!"
+              className="focus:outline-none placeholder:italic placeholder:text-sm placeholder:font-normal 
+            bg-slate-900 rounded-sm 
+            w-full p-2 font-normal"
+            />
+            <button
+              className="bg-primary-orange text-black rounded-sm"
+              type="submit"
+            >
+              Send
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
