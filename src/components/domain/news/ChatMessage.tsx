482f2ebcaff2fe44de6011d43bb98c27acb0db22
newdana01 <newdana01@gmail.com>
2025-08-29 17:16:32 +0900

    feat: ChatMessage 컴포넌트 구현. 봇/유저에 따라 다른 스타일 적용

---
+++ src/components/domain/news/ChatMessage.tsx
@@ -0,0 +1,21 @@
+import AIWriter from "react-aiwriter";
+
+interface ChatMessageProps {
+  message: string;
+  isUser: boolean;
+}
+
+export default function ChatMessage({ message, isUser }: ChatMessageProps) {
+  return (
+    <article
+      className={`${isUser ? "bg-slate-800" : "bg-slate-700"} 
+      ${isUser ? "float-right" : "float-left"} 
+      font-normal p-2 mb-2 rounded-md w-72
+      `}
+    >
+      <header className="text-slate-500">{isUser ? "User" : "Bot"}</header>
+      {isUser && <p>{message}</p>}
+      {!isUser && <AIWriter>{message}</AIWriter>}
+    </article>
+  );
+}
