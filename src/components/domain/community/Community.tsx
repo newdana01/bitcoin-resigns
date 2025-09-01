2afa631ff695774c5a308e11df0aa051b1cdb600
newdana01 <newdana01@gmail.com>
2025-09-01 15:43:56 +0900

    feat: Community 하위 컴포넌트 추가

--- src/components/domain/community/Community.tsx
+++ src/components/domain/community/Community.tsx
@@ -1,9 +1,11 @@
+import Donate from "./Donate";
 import HolderList from "./HolderList";
 
 export default function Community() {
   return (
     <div className="grid-container">
       <HolderList />
+      <Donate />
     </div>
   );
 }
