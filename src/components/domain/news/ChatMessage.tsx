2b032a96bd64a37218354fcbf010d5d86850215d
newdana01 <newdana01@gmail.com>
2025-09-01 16:09:23 +0900

    feat: 채팅 메시지 정렬 오류 해결

--- src/components/domain/news/ChatMessage.tsx
+++ src/components/domain/news/ChatMessage.tsx
@@ -7,15 +7,19 @@ interface ChatMessageProps {
 
 export default function ChatMessage({ message, isUser }: ChatMessageProps) {
   return (
-    <article
-      className={`${isUser ? "bg-slate-300 dark:bg-slate-800" : "bg-slate-400 dark:bg-slate-700"} 
-      ${isUser ? "float-right" : "float-left"} 
+    <div className="grid">
+      <div
+        className={`${isUser ? "bg-slate-300 dark:bg-slate-800" : "bg-slate-400 dark:bg-slate-700"} 
+      ${isUser ? "justify-self-end" : "justify-self-start"} 
       font-normal p-2 mb-2 rounded-md w-72
       `}
-    >
-      <header className="dark:text-slate-500">{isUser ? "User" : "Bot"}</header>
-      {isUser && <p>{message}</p>}
-      {!isUser && <AIWriter>{message}</AIWriter>}
-    </article>
+      >
+        <header className="dark:text-slate-500">
+          {isUser ? "User" : "Bot"}
+        </header>
+        {isUser && <p>{message}</p>}
+        {!isUser && <AIWriter>{message}</AIWriter>}
+      </div>
+    </div>
   );
 }
