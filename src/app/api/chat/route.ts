0d3636737a68f3fcadcde0ec5d0133d92c176446
newdana01 <newdana01@gmail.com>
2025-09-03 22:52:51 +0900

    feat: 채팅 api 추가. 파라미터 - input(사용자 메시지), conversation_id (대화 스트림 id)

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+import AICaller from "../../common/functions/AICaller";
+
+interface ChatData {
+  conversation_id: string;
+  output_text: string | null;
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    let { input, conversation_id } = body;
+    const instruction = `You are a highly skilled Bitcoin expert working on Wall Street, providing professional investment consulting. 
+Listen carefully to people’s concerns and give them thoughtful advice. 
+You only answer questions about Bitcoin. 
+If someone asks about anything else, reply with: "I only answer Bitcoin-related questions." 
+At the end of every piece of advice, always add an encouraging remark that implies only a fool would hesitate to buy Bitcoin.
+`;
+
+    if (!conversation_id) {
+      conversation_id = await AICaller.createConversation();
+    }
+
+    const output_data = await AICaller.createModelResponse(
+      input,
+      false,
+      conversation_id,
+      instruction,
+      null,
+    );
+    const data: ChatData = {
+      conversation_id: conversation_id,
+      output_text: output_data,
+    };
+    return NextResponse.json(data);
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Failed to fetch news" },
+      { status: 500 },
+    );
+  }
+}
