ceb5a17c9bf6e73c62a22bcdc6053fe89207e16a
newdana01 <newdana01@gmail.com>
2025-09-03 17:50:33 +0900

    feat: hydration 에러 해결

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import Layout from "../components/layout/Layout";
+import dynamic from "next/dynamic";
 
-// const App = dynamic(() => import("../App"), { ssr: false });
+const App = dynamic(() => import("../App"), { ssr: false });
 
 export function ClientOnly() {
-  return <Layout></Layout>;
+  return <App />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import "../App.css";
 import { ClientOnly } from "./client";
 
